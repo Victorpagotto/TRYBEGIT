c5de5e5d81edf45a950fdca8ac04bdf742d5c0e5
VictorPagotto <victor.pagotto12@gmail.com>
2022-03-08 16:01:09 -0300

    Terminado a parte 6.

--- Module-1/Block-4/Introdução-a-JS-e-Lógica-de-Programação/exercicio.js
+++ Module-1/Block-4/Introdução-a-JS-e-Lógica-de-Programação/exercicio.js
@@ -58,8 +58,38 @@ function angleTriangle (angles){
         return false;
     }
 }
-
+function chessPiece (chess){
+    switch (chess.toLowerCase()){
+        case 'king':
+            console.log('One square in any direction, so long as that square is not attacked by an enemy piece. Additionally, kings are able to make a special move, known as castling.');
+            break;
+        case 'queen':
+            console.log('Diagonally, horizontally, or vertically any number of squares. They are unable to jump over pieces.');
+            break;
+        case 'rook':
+            console.log('Horizontally or vertically any number of squares. They are unable to jump over pieces. Rooks move when the king castles.');
+            break;
+        case 'knight':
+            console.log('In an ‘L’ shape’: two squares in a horizontal or vertical direction, then move one square horizontally or vertically. They are the only piece able to jump over other pieces.');
+            break;
+        case 'bishop':
+            console.log('Diagonally any number of squares. They are unable to jump over pieces.');
+            break;
+        case 'pawn':
+            console.log('Vertically forward one square, with the option to move two squares if they have not yet moved. Pawns are the only piece to capture different to how they move. The pawns capture one square diagonally in a forward direction. Pawns are unable to move backward on captures or moves. Upon reaching the other side of the board a pawn promotes into any other piece, except for a king. Additionally, pawns can make a special move named En Passant.');
+            break;
+        default:
+            console.log('This isnt a chess piece, or you misstyped. Make sure you typed correctly.')
+    }
+}
+console.log('------------Exercicio------------');
 allOperators(a,b);
+console.log('------------Exercicio------------');
 console.log(biggestNumber(arr));
+console.log('------------Exercicio------------');
 console.log(angleTriangle(angles));
+console.log('------------Exercicio------------');
 positiveOrNegative(c);
+console.log('------------Exercicio------------');
+chessPiece('Queen');
+console.log('------------Exercicio------------');
