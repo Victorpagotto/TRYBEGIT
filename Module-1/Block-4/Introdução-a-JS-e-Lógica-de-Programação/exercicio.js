8406c8226cdab46da03a73cc1d8f33f076959c05
VictorPagotto <victor.pagotto12@gmail.com>
2022-03-08 16:37:42 -0300

    Terminado ate o exercicio 10.

--- Module-1/Block-4/Introdução-a-JS-e-Lógica-de-Programação/exercicio.js
+++ Module-1/Block-4/Introdução-a-JS-e-Lógica-de-Programação/exercicio.js
@@ -2,8 +2,12 @@ let a=3;
 let b=2;
 let c=3;
 const angles= [40, 80, 60];
-
+let grade = 75;
 let arr=[1,4,5,7,12,25,63,44,22,-42,75,4,-1000];
+numberSet = [1213, 123, 533];
+let cost = 5;
+let sellPrice = 9;
+let produNumber = 1000;
 
 function sumUp (angles){
     let sumUp=null;
@@ -82,6 +86,49 @@ function chessPiece (chess){
             console.log('This isnt a chess piece, or you misstyped. Make sure you typed correctly.')
     }
 }
+function gradeCalculator(grade){
+    grade = Math.floor(grade/10);
+    switch (grade){
+        case 9:
+            return 'A';
+        case 8:
+            return 'B';
+        case 7:
+            return 'C';
+        case 6:
+            return 'D';
+        case 5:
+            return 'E';
+        default:
+            if (grade<5 && grade>=0){
+            return 'F';
+            }else{
+                return 'Error. Grade is not a real grade value.'
+            }
+    }
+}
+
+function pairOrNotPair (numberSet){
+    for (let i=0; i<numberSet.length; i++){
+        if (numberSet[i]%2===0){
+            return true;
+        }
+    }
+    return false;
+}
+function NotPair (numberSet){
+    for (let i=0; i<numberSet.length; i++){
+        if (numberSet[i]%2!==0){
+            return true;
+        }
+    }
+    return false;
+}
+function profiting(cost, sellPrice, produNumber){
+    cost = (cost/5) + cost;
+    return (sellPrice - cost)*produNumber;
+}
+
 console.log('------------Exercicio------------');
 allOperators(a,b);
 console.log('------------Exercicio------------');
@@ -92,4 +139,14 @@ console.log('------------Exercicio------------');
 positiveOrNegative(c);
 console.log('------------Exercicio------------');
 chessPiece('Queen');
+console.log('------------Exercicio------------');
+console.log(gradeCalculator(grade));
+console.log('------------Exercicio------------');
+console.log(pairOrNotPair(numberSet));
+console.log('------------Exercicio------------');
+console.log(NotPair(numberSet));
+console.log('------------Exercicio------------');
+console.log(profiting(cost, sellPrice, produNumber));
+console.log('------------Exercicio------------');
+console.log(taxing(salary));
 console.log('------------Exercicio------------');
