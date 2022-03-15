6ba1c32b9db151ef9db92bfc60614fc8bfd997e1
VictorPagotto <victor.pagotto12@gmail.com>
2022-03-15 20:13:46 -0300

    Terminada a primeira parte.

--- Module-1/Block-5/JS: trabalhando-com-elementos/index.js
+++ Module-1/Block-5/JS: trabalhando-com-elementos/index.js
@@ -10,8 +10,9 @@ function classer(element = '', attribute, type = 'class') {
 let firstH1 = tagWText('h1', 'Exercício 5.2 - JavaScript DOM');
 let main = tagWText('main');
 classer(main, 'main-content');
-let section1 = tagWText('section', '', main);
 //Section 1 center
+let section1 = tagWText('section', '', main);
+tagWText('p','Algum texto para preencher espaço.', section1);
 classer(section1, 'center-content');
 //Section 2 left
 let section2 = tagWText('section', '', main);
@@ -22,4 +23,13 @@ classer(section2, 'left-content');
 //section 3 right
 let section3 = tagWText('section', '', main);
 classer(section3, 'right-content');
-tagWText('p','Algum texto para preencher espaço.', section1);
+let list = tagWText('ul', '', section3);
+for (let i = 1; i < 11; i += 1) {
+    tagWText('li', `${i}`, list);
+}
+//3h's
+tagWText('h3','A h3', main);
+tagWText('h3','A h3', main);
+tagWText('h3','A h3', main);
+
+
