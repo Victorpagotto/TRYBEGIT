d9e7f09936b790f382f0921d2881b820e9cff8da
VictorPagotto <victor.pagotto12@gmail.com>
2022-03-24 16:16:17 -0300

    Feito exercicios de fixing.

---
+++ Module-1/Block-6/HTML&CSS:Forms/fixing/script.js
@@ -0,0 +1,15 @@
+const INPUT_TEXT = document.querySelector("#input-text");
+const INPUT_CHECKBOX = document.querySelector("#input-checkbox");
+const HREF_LINK = document.querySelector("#href");
+
+INPUT_TEXT.addEventListener("keypress", (event) => {
+  if (event.key !== 'a') {
+    event.preventDefault();
+  }
+});
+INPUT_CHECKBOX.addEventListener('click', (event) => {
+  event.preventDefault();
+});
+HREF_LINK.addEventListener('click', (event) => {
+  event.preventDefault();
+});
