fc7358d8c5a7f95481a9b6a6c705f9da870a474b
VictorPagotto <victor.pagotto12@gmail.com>
2022-04-06 05:07:27 -0300

    Quase terminado. Ainda faltam os testes do exercicio bonus.

--- Module-1/Block-7/JS_ES6:PrimeirosPassosEmJest/exercicio/script.js
+++ Module-1/Block-7/JS_ES6:PrimeirosPassosEmJest/exercicio/script.js
@@ -1,3 +1,81 @@
+const techList = [{
+  tech: 'CSS',
+  name: 'Lucas'
+},
+{
+  tech: 'HTML',
+  name: 'João'
+},
+{
+  tech: 'JavaScript',
+  name: 'Lucas'
+},
+{
+  tech: 'React',
+  name: 'Pedro'
+},
+{
+  tech: 'React',
+  name: 'Lucas'
+},
+{
+  tech: 'Jest',
+  name: 'Carlos'
+}];
+let codes = [{
+  letter: 'a',
+  number: '1',
+}, {
+  letter: 'e',
+  number: '2',
+}, {
+  letter: 'i',
+  number: '3',
+}, {
+  letter: 'o',
+  number: '4',
+}, {
+  letter: 'u',
+  number: '5',
+}];
+const professionalBoard = [
+  {
+    id: '8579-6',
+    firstName: 'Ana',
+    lastName: 'Gates',
+    specialities: ['UX', 'Design'],
+  },
+  {
+    id: '5569-4',
+    firstName: 'George',
+    lastName: 'Jobs',
+    specialities: ['Frontend', 'Redux', 'React', 'CSS'],
+  },
+  {
+    id: '4456-4',
+    firstName: 'Leila',
+    lastName: 'Zuckerberg',
+    specialities: ['Context API', 'RTL', 'Bootstrap'],
+  },
+  {
+    id: '1256-4',
+    firstName: 'Linda',
+    lastName: 'Bezos',
+    specialities: ['Hooks', 'Context API', 'Tailwind CSS'],
+  },
+  {
+    id: '9852-2-2',
+    firstName: 'Jeff',
+    lastName: 'Cook',
+    specialities: ['Ruby', 'SQL'],
+  },
+  {
+    id: '4678-2',
+    firstName: 'Paul',
+    lastName: 'Dodds',
+    specialities: ['Backend'],
+  },
+];
 const sum = (a, b) => {
   if (typeof a !== 'number' || typeof b !== 'number') {
     throw new Error('parameters must be numbers');
@@ -14,9 +92,168 @@ function myRemove(arr, item) {
   }
   return newArr;
 }
+function myFizzBuzz(num) {
+  if (typeof num !== 'number') return false;
+  if (num % 3 === 0 && num % 5 === 0) return 'fizzbuzz';
+  if (num % 3 === 0) return 'fizz';
+  if (num % 5 === 0) return 'buzz';
+  return num;
+}
+//Code Enconde
+function code(char) {
+  for (let i = 0; i < codes.length; i += 1) {
+    if (char === codes[i].letter) {
+      return codes[i].number;
+    }
+  }
+  return char;
+}
+function uncode(char) {
+  for (let i = 0; i < codes.length; i += 1) {
+    if (char === codes[i].number) {
+      return codes[i].letter;
+    }
+  }
+  return char;
+}
 
+function encode(arr) {
+  let encoded = [];
+  for (let i = 0; i < arr.length; i += 1) {
+    encoded[i] = code(arr[i]);
+  }
+  return encoded.join('');
+}
+function decode(arr) {
+  let decoded = [];
+  for (let i = 0; i < arr.length; i += 1) {
+    decoded[i] = uncode(arr[i]);
+  }
+  return decoded.join('');
+}
+//TechList
+function positioner(arr) {
+  for (let i in arr) {
+    arr[i].position = i;
+  }
+  return arr;
+}
+
+function createsNameList(arr) {
+const nameList = [];
+for (let i in arr) {
+  nameList.push(arr[i].name);
+}
+return nameList;
+}
+function createsSort(arr) {
+  const techSort = [];
+  const techSortArr = [];
+  for (let i in arr) {
+    techSort[i] = arr[i].tech;
+  }
+  techSort.sort();
+  for (let i in techSort) {
+    techSortArr[i] = [techSort[i]];
+  }
+  return techSortArr;
+}
+function attributesPosition(techSort, techPosition) {
+  for (let i = 0; i < techSort.length; i += 1) {
+    for (let x = 0; x < techPosition.length; x += 1) {
+      if (techSort[i][0] === techPosition[x].tech) {
+        techSort[i][1] = techPosition[x].position;
+        techPosition[x].tech = '';
+        break;
+      }
+    }
+  }
+  return techSort;
+}
+function arrObjectSorter(arr) {
+  const nameList = createsNameList(arr);
+  const techSort = attributesPosition(createsSort(arr), positioner(arr));
+  const techList = [];
+  for (let i in techSort) {
+    const object = {
+      tech: techSort[i][0],
+      name: nameList[techSort[i][1]]
+    }
+    techList.push(object);
+  }
+  return techList;
+}
+function createTechList(arr, name) {
+  if (arr.length > 0) {
+    const techList = [];
+    for (let i in arr) {
+      const object = {
+        tech: arr[i],
+        name: name
+      }
+      techList.push(object);
+    }
+    return arrObjectSorter(techList);
+  }
+  return 'Vazio!';
+}
+//Hydrate
+function numIdentifier(char) {
+  let numSet = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
+  for (let i = 0; i < numSet.length; i += 1) {
+    if (parseInt(char, 10) === numSet[i]) return true;
+  }
+  return false;
+}
+function extractor(arr) {
+  let nums = [];
+  for (let i = 0; i < arr.length; i += 1) {
+    if (numIdentifier(arr[i])) nums.push(parseInt(arr[i], 10));
+  }
+  return nums;
+}
+function hydrate(arr) {
+  let numSet = extractor(arr);
+  let sumUp = 0;
+  for (let i in numSet) sumUp += numSet[i];
+  if (sumUp > 1) return `${sumUp} copos de água`;
+  return `${sumUp} copo de água`;
+}
+//Search Id
+function findsEmployee(id) {
+  for (let i in professionalBoard) {
+    if (id === professionalBoard[i].id) return professionalBoard[i];
+  }
+  return false;
+}
+function createsInformation(detail, entries) {
+  const information = {};
+  for (let i in detail) {
+    information[detail[i]] = 'Informação não disponível';
+    for (let x in entries) {
+      if (entries[x][0] === detail[i]) {
+        information[detail[i]] = entries[x][1];
+      }
+    }
+  }
+  return information;
+}
+function searchEmployee(id, detail) {
+  const employee = findsEmployee(id);
+  if (employee === false) return 'ID não identificada';
+  const entries = Object.entries(employee);
+  return createsInformation(detail, entries);
+}
 console.log(sum(4, 5));
+console.log(createTechList(['JavaScript', 'React', 'Jest', 'HTML', 'CSS'], 'Lucas'));
+console.log('---------------------');
+console.log(searchEmployee('4456-4', ['specialities']));
 module.exports = {
   sum: sum,
-  myRemove: myRemove
+  myRemove: myRemove,
+  myFizzBuzz: myFizzBuzz,
+  code: encode,
+  decode: decode,
+  createTechList: createTechList,
+  hydrate: hydrate,
 }
--- Module-1/Block-7/JS_ES6:PrimeirosPassosEmJest/exercicio/script.test.js
+++ Module-1/Block-7/JS_ES6:PrimeirosPassosEmJest/exercicio/script.test.js
@@ -27,3 +27,108 @@ describe('A função myRemove(arr, item) recebe um array arr e retorna uma cópi
     expect(functions.myRemove(arr, 5)).toEqual(arr);
   });
 });
+
+describe('Função myFizzBuzz(num) recebe um número num e retorna "fizzbuzz" se o número for divisível por 3 e 5 , retorna "fizz" se for divisível apenas por 3 , retorna "buzz" se divisível apenas por 5 , retorna o próprio número caso não seja divisível por 3 ou 5 e retorna false caso num não seja um número', () => {
+  it('Faz uma chamada com um número divisível por 3 e 5 e verifica se o retorno é fizzbuzz', () => {
+    expect(functions.myFizzBuzz(15)).toBe('fizzbuzz');
+  });
+  it('Faça uma chamada com um número divisível por 3 e verifique se o retorno é fizz', () => {
+    expect(functions.myFizzBuzz(6)).toBe('fizz');
+  });
+  it('Faz uma chamada com um número divisível por 3 e verifique se o retorno é  buzz', () => {
+    expect(functions.myFizzBuzz(10)).toBe('buzz');
+  });
+  it('Faz uma chamada com um número que não é divisível por 3 ou 5 e verifique se o retorno é o próprio número', () => {
+    expect(functions.myFizzBuzz(4)).toBe(4);
+  });
+  it('Faz uma chamada com um parâmetro que não é um número e verifica se o retorno é false', () => {
+    expect(functions.myFizzBuzz('fizz')).toBe(false);
+  });
+});
+
+describe('Verifica as funções encode e decode.', () => {
+  it('Verifica se code é uma função.', () => {
+    expect(() => {functions.code}).toBeInstanceOf(Function);
+  });
+  it('Verifica se decode é uma função.', () => {
+    expect(() => {functions.decode}).toBeInstanceOf(Function);
+  });
+  it('Para a função encode, testa se as vogais a, e, i, o, u são convertidas em 1, 2, 3, 4 e 5, respectivamente.', () => {
+    expect(functions.code('a, e, i, o, u')).toEqual('1, 2, 3, 4, 5');
+  });
+  it('Para a função decode, testa se as vogais 1, 2, 3, 4 e 5 são convertidas em a, e, i, o e u, respectivamente.', () => {
+    expect(functions.decode('1, 2, 3, 4, 5')).toEqual('a, e, i, o, u');
+  });
+  it('Verifica mais um caso de encode.', () => {
+    expect(functions.code('adsa, essdu, i, oesd, u')).toEqual('1ds1, 2ssd5, 3, 42sd, 5');
+  });
+  it('Verifica mais um caso de decode.', () => {
+    expect(functions.decode('1ds1, 2ssd5, 3, 42sd, 5')).toEqual('adsa, essdu, i, oesd, u');
+  });
+  const string = '1ds1, 2ssd5, 3, 42sd, 5';
+  it('Verifica se a length não muda.', () => {
+    expect(functions.decode(string)).toHaveLength(string.length);
+  });
+});
+
+const techList = functions.createTechList;
+
+describe('Testa a função techList', () => {
+  it('Testa se a função techList é definida', () => {
+    expect(techList).toBeDefined();
+  });
+  it('Testa se techList é uma função', () => {
+    expect(typeof techList).toBe('function');
+  });
+  it('Lista com 5 tecnologias deve retornar uma lista de objetos ordenados', () => {
+    expect(techList(['React', 'Jest', 'HTML', 'CSS', 'JavaScript'], 'Lucas')).toEqual([
+      {
+        tech: 'CSS',
+        name: 'Lucas'
+      },
+      {
+        tech: 'HTML',
+        name: 'Lucas'
+      },
+      {
+        tech: 'JavaScript',
+        name: 'Lucas'
+      },
+      {
+        tech: 'Jest',
+        name: 'Lucas'
+      },
+      {
+        tech: 'React',
+        name: 'Lucas'
+      }
+    ]);
+  });
+  it('Lista com 0 tecnologias deve retornar uma mensagem de erro "Vazio!"', () => {
+    expect(techList([], 'Lucas')).toBe('Vazio!');
+  });
+});
+
+const hydrate = functions.hydrate;
+
+describe('Testa a função hydrate', () => {
+  it('Testa se a função hydrate é definida', () => {
+    expect(hydrate).toBeDefined();
+  });
+  it('Testa se hydrate é uma função', () => {
+    expect(typeof hydrate).toBe('function');
+  });
+  it('Ao receber uma string retorne a sugestão de quantos copos de água deve-se beber', () => {
+    expect(hydrate('1 cerveja')).toBe('1 copo de água');
+    expect(hydrate('1 cachaça, 5 cervejas e 1 copo de vinho')).toBe('7 copos de água');
+    expect(hydrate('2 shots de tequila, 2 cervejas e 1 corote')).toBe('5 copos de água');
+    expect(hydrate('1 copo de catuaba, 1 cervejas e 1 copo de vinho')).toBe('3 copos de água');
+    expect(hydrate('4 caipirinhas e 2 cervejas')).toBe('6 copos de água');
+  });
+});
+
+// describe('', () => {
+//   it('', () => {
+//     expect(functions.)
+//   });
+// });
