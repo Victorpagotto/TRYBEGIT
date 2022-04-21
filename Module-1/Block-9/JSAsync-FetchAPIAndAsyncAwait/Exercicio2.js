4ca45cbfddb25e4a6893b2f6051bde79675de2ae
VictorPagotto <victor.pagotto12@gmail.com>
2022-04-21 01:27:30 -0300

    Exercicio bonus feito.

--- Module-1/Block-9/JSAsync-FetchAPIAndAsyncAwait/Exercicio2.js
+++ Module-1/Block-9/JSAsync-FetchAPIAndAsyncAwait/Exercicio2.js
@@ -11,16 +11,30 @@ async function getFetch() {
 }
 const list = document.getElementById('moedas');
 
+async function getCurrency(curr1, curr2, money) {
+  const coin = await fetch(`https://cdn.jsdelivr.net/gh/fawazahmed0/currency-api@1/latest//currencies/${curr1}/${curr2}.json`)
+    .then((resolve) => resolve.json())
+    .then((resolve) => parseFloat(money) * resolve.brl);
+    return coin;
+}
 
-getFetch().then((coins) => {
-  return coins.map((coin, index) => {
-    if (index < 10) return coin;
+async function getCriptValues() {
+ const data = await getFetch()
+  .then((resolve) => resolve.map((coin, index) => {
+  if (index < 10) return coin;
   })
   .filter((coin) => coin !== undefined)
   .sort((a, b) =>  b.priceUsd - a.priceUsd)
-  .map((coin, i) => `${i + 1}-${coin.name}`);
-}).then((response) => response.forEach((moeda) => {
+  .map(async (coin, i) => {
+    const value = await getCurrency('usd', 'brl', coin.priceUsd);
+    return(`${i + 1}-${coin.name}:  R$${Number.parseFloat(value).toFixed(3)}`);
+  }));
+  return Promise.all(data);
+}
+
+getCriptValues().then((response) => response.forEach((currency) => {
   const li = document.createElement('li');
-  li.innerHTML = moeda;
+  li.innerHTML = currency;
   list.appendChild(li);
-}));
+})
+);
